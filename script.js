8a7bf31771718449d4f507d11b152f6caf2c5520
Amanda Bartolo <mandssb@gmail.com>
2022-05-09 10:13:11 -0300

    Input date (v2)

--- script.js
+++ script.js
@@ -3,18 +3,15 @@ const hoursEl = document.querySelector("#hours")
 const minutesEl = document.querySelector("#minutes")
 const secondsEl = document.querySelector("#seconds")
 
-const birthdayDate = "11 jan 2023" 
-
 function countdown(){
-    const futureDate = new Date(birthdayDate)
-    const currentDate = new Date()
+    var inputDate = new Date(document.querySelector("#input-date").value)
+    var currentDate = new Date()
+    var rest = inputDate - currentDate
 
-    const totalSeconds = (futureDate - currentDate) / 1000
-
-    const restDays = Math.floor(totalSeconds / 3600 / 24)
-    const restHours = Math.floor(totalSeconds / 3600) % 24
-    const restMinutes = Math.floor(totalSeconds / 60) % 60
-    const restSeconds = Math.floor(totalSeconds % 60)
+    var restDays = Math.floor(rest / (1000 * 60 * 60 * 24))
+    var restHours = Math.floor((rest % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+    var restMinutes = Math.floor((rest % (1000 * 60 * 60)) / (1000 * 60))
+    var restSeconds = Math.floor((rest % (1000 * 60)) / 1000)
 
     daysEl.textContent = restDays
     hoursEl.textContent = restHours
@@ -23,5 +20,3 @@ function countdown(){
 
     setInterval(countdown, 1000)
 }
-
-countdown()
